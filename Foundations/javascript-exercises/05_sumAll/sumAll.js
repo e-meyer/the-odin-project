b0688d7149173de00c5c81e0c7f447b2ab2512e4
e-meyer <meyeredy20@gmail.com>
2022-07-12 20:56:34 -0300

    Add solution to sumAll exercise

--- Foundations/javascript-exercises/05_sumAll/sumAll.js
+++ Foundations/javascript-exercises/05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+    if(firstNumber < 0 || secondNumber < 0) return 'ERROR'
+    else if(isNaN(firstNumber) || isNaN(secondNumber)) return 'ERROR'
+    else if(typeof firstNumber === 'string' || typeof secondNumber === 'string') return 'ERROR'
 
+
+    let sum = 0;
+
+    if(firstNumber > secondNumber){
+        for(let i = secondNumber; i <= firstNumber; i++){
+            sum += i
+        }
+    } else if(firstNumber < secondNumber){
+        for(let i = firstNumber; i <= secondNumber; i++){ 
+            sum += i
+        }
+    }
+    else 
+        return firstNumber
+
+    return sum
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
