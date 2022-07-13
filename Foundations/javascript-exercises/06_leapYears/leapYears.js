2ce866559854c18c674a06f2d7331231150aec32
e-meyer <meyeredy20@gmail.com>
2022-07-12 21:16:09 -0300

    Add solution to leapYears exercise

--- Foundations/javascript-exercises/06_leapYears/leapYears.js
+++ Foundations/javascript-exercises/06_leapYears/leapYears.js
@@ -1,4 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    
+    if(year % 4 == 0){                      
+        if(year % 100 == 0){                // if the year is divisible by 100
+            if(year % 400 == 0){            // then it needs to be divisible by 400 to be a leap year
+                return true;
+            }
+        }   else if (year % 100 != 0){      // if the year is divisible by 4 and not by 100, it's definetly a leap year
+                return true;
+        }
+    }
+    return false;                           // if none of the statements are true, then it returns false
 
 };
 
