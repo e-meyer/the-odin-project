0878896a14652968bc89cf8ea6522e726a9c433c
e-meyer <meyeredy20@gmail.com>
2022-07-12 21:29:14 -0300

    Add solution to tempConversion  exercise

--- Foundations/javascript-exercises/07_tempConversion/tempConversion.js
+++ Foundations/javascript-exercises/07_tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const ftoc = function() {
+const ftoc = function(fahrenheitTemperature) {
+
+  let finalTemperature = (fahrenheitTemperature - 32) * (5/9); 
+
+  return Number(finalTemperature.toFixed(1));                 // function in number was used because sometimes it would be returned as a string
 
 };
 
-const ctof = function() {
+const ctof = function(celciusTemperature) {
+  
+  let finalTemperature = (celciusTemperature * (9/5)) +32;    // function in number was used because sometimes it would be returned as a string
 
+  return Number(finalTemperature.toFixed(1));
 };
 
 // Do not edit below this line
